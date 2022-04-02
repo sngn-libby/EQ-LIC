e4491a441e8839918ac63f4e12d3df5b3038a9fc
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2022-04-02 23:39:30 +0900

    Update utils.py

--- codes/quan/utils.py
+++ codes/quan/utils.py
@@ -20,7 +20,7 @@ def quantizer(default_cfg, this_cfg=None):
         q = OffsetLSQ
     elif target_cfg['mode'] == 'offset_lsq_plus':
         q = OffsetLSQPlus
-    elif target_cfg['mode'] == 'lsq':
+    elif target_cfg['mode'] == 'liq':
         q = LiqQuan
     else:
         raise ValueError('Cannot find quantizer `%s`', target_cfg['mode'])
