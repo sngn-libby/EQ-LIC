7fce36f96a6e6053d7c7c83d296d249202cd4b5a
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2022-08-24 22:07:05 +0900

    Update train.py

--- codes/examples/train.py
+++ codes/examples/train.py
@@ -495,7 +495,7 @@ def main(argv):
     optimizer, aux_optimizer = configure_optimizers(net, args)
     # lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
     # lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", factor=0.3, patience=20, verbose=True)
-    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[900, ], gamma=0.1)
+    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[950, ], gamma=0.1)
     # lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.25)
     criterion = RateDistortionLoss(lmbda=args.lmbda, metrics=args.metrics)
 
