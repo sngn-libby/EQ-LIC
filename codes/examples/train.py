0212dc32dd0fe6d448d0101d58649ece9b6134bb
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2022-10-10 11:03:03 +0900

    Update train.py

--- codes/examples/train.py
+++ codes/examples/train.py
@@ -292,8 +292,10 @@ def test_epoch(epoch, test_dataloader, model, criterion, save_dir, logger_val, t
 def save_checkpoint(state, is_best, filename="checkpoint.pth.tar"):
     # torch.save(state, filename)
     if is_best:
-        dest_filename = filename.replace(filename.split('/')[-1], "checkpoint_best_loss.pth.tar")
+        dest_filename = filename.replace(filename.split('\\')[-1], "checkpoint_best_loss.pth.tar")
         torch.save(state, dest_filename)
+        print(filename)
+        print(dest_filename)
         # shutil.copyfile(filename, dest_filename)
 
 
@@ -534,18 +536,18 @@ def main(argv):
 
     for epoch in range(start_epoch, args.epochs):
         logger_train.info(f"Learning rate: {optimizer.param_groups[0]['lr']}")
-        current_step = train_one_epoch(
-            net,
-            criterion,
-            train_dataloader,
-            optimizer,
-            aux_optimizer,
-            epoch,
-            args.clip_max_norm,
-            logger_train,
-            tb_logger,
-            current_step
-        )
+        # current_step = train_one_epoch(
+        #     net,
+        #     criterion,
+        #     train_dataloader,
+        #     optimizer,
+        #     aux_optimizer,
+        #     epoch,
+        #     args.clip_max_norm,
+        #     logger_train,
+        #     tb_logger,
+        #     current_step
+        # )
 
         save_dir = os.path.join('../experiments', args.experiment, 'val_images', '%03d' % (epoch + 1))
         loss = test_epoch(epoch, test_dataloader, net, criterion, save_dir, logger_val, tb_logger)
@@ -564,7 +566,7 @@ def main(argv):
                     "lr_scheduler": lr_scheduler.state_dict(),
                 },
                 is_best,
-                os.path.join('../experiments', args.experiment, 'checkpoints', "checkpoint_%03d.pth.tar" % (epoch + 1))
+                os.path.join('..\\experiments', args.experiment, 'checkpoints', "checkpoint_%03d.pth.tar" % (epoch + 1))
             )
             if is_best:
                 logger_val.info('best checkpoint saved.')
